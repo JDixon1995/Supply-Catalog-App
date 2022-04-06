00dd1aa6471a7025c8637f2b88c62523833a3a94
John D <jddhalogamer@gmail.com>
2022-04-06 18:44:15 -0500

    modelled Schema for Sale data.

---
+++ server/models/salesModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const salesSchema = new mongoose.Schema({
+	saleDate: {
+		type: String,
+		required: true,
+	},
+	items: {
+		type: Array
+	},
+	storeLocation: {
+		type: String,
+		required: true,
+	},
+	customer: {
+		type: Object,
+		required: true,
+	},
+	couponUse: {
+		type: Boolean,
+		required: true,
+	},
+	purchaseMethod: {
+		type: String,
+		required: true,
+	},
+})
+
+module.exports = mongoose.model('Sale', salesSchema)
