5e8be27344d8529b5336896e572e30e4f661d220
John D <jddhalogamer@gmail.com>
2022-04-07 16:51:27 -0500

    added routes to query by purchaseMethod.

--- server/controllers/salesController.js
+++ server/controllers/salesController.js
@@ -1,11 +1,1 @@
-const Sale = require('../models/salesModel')
-
 // Get all sales
-const getSales = async (req, res) => {
-	const sales = await Sale.find()
-	res.json(sales)
-}
-
-module.exports = {
-	getSales
-}
--- server/routes/saleRoutes.js
+++ server/routes/saleRoutes.js
@@ -1,4 +1,3 @@
-const mongodb = require('mongodb')
 const express = require('express')
 const router = express.Router()
 const Sale = require('../models/salesModel')
@@ -9,9 +8,20 @@ router.get('/', async (req, res) => {
 	res.status(200).json(sales)
 })
 
+router.get('/online', async (req, res) => {
+	const onlineSales = await Sale.find({ purchaseMethod : "Online" })
+	res.status(200).json(onlineSales)
+})
+
+router.get('/store', async (req, res) => {
+	const storeSales = await Sale.find({ purchaseMethod : "In store" })
+	res.status(200).json(storeSales)
+})
+
 router.get('/:id', async (req, res) => {
 	const sale = await Sale.findById({ _id: req.params.id})
 	res.status(200).json(sale)
 })
+
 
 module.exports = router
